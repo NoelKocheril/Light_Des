bcfaa9514adc3a58287e7231b20f24b24af21a1c
NoelKocheril <NoelKocheril@hotmail.com>
2018-03-12 11:07:15 -0400

    March 12

---
+++ cmake-build-debug/CMakeFiles/bin_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bin_test.dir/bin_test.cpp.obj"
+  "bin_test.pdb"
+  "bin_test.exe"
+  "bin_test.exe.manifest"
+  "libbin_test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bin_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
